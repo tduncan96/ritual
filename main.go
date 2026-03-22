e1ce98de29ba0d87a9dda8a6054ab7f682a37331
tduncan <ttduncan96@live.com>
2026-03-21 22:32:01 -0700

    Starting to get the sqlite database stuff set up.

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"net/http"
 	"html/template"
 	"log"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+var pool *sql.DB
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	
+	// Main Page
 	t, err := template.New("home.html").ParseFiles("templates/home.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -22,6 +28,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	http.HandleFunc("/", handler)
+
+	http.HandleFunc("/", handler) //Main Page
+	
+	
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
